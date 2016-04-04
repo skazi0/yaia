2e46cd5c8ad7c7fd8d26abb316fd2794322b34bb
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2016-04-04 13:52:00 +0200

    added flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.bcrypt import Bcrypt
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 from app.config import BaseConfig
 
 
@@ -12,6 +13,15 @@ app.config.from_object(BaseConfig)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# skz: how to handle this with angular?
+#login_manager.login_view = 'login'
+
+#@login_manager.user_loader
+#def load_user(id):
+#    return User.query.get(int(id))
 
 # routes
 
