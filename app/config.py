36bb09b4e2467dab9521e47d88e30a4042c88480
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2016-04-10 22:05:21 +0200

    - lowered DB pool recycle time to avoid timeouts on MySQL connections - renamed session cookie to avoid conflicts in shared reverse proxy setups

--- app/config.py
+++ app/config.py
@@ -4,3 +4,5 @@ class BaseConfig(object):
     BCRYPT_LOG_ROUNDS = 13
     SQLALCHEMY_DATABASE_URI = 'mysql://user:pass@localhost/yaia'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_RECYCLE = 500
+    SESSION_COOKIE_NAME = 'yaiasession'
