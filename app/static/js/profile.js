1261e587385fcde1eb3a951d4d066d37ef2fc518
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2016-04-16 07:24:12 +0200

    - switched user nav to dropdown - added profile view stub

---
+++ app/static/js/profile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var yaiaProfile = angular.module('yaia.profile', ['ui.router']);
+
+yaiaProfile.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('user.profile', {url: '/profile', templateUrl: 'static/views/partials/profile.html', controller: 'ProfileCtrl'});
+}]);
+
+yaiaProfile.controller('ProfileCtrl', ['$scope', function($scope) {
+    $scope.todo = 'profile information and settings';
+}]);
