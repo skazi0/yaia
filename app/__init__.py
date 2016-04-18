7a1758f49766b927f80717659d82f3d23fa7269b
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2016-04-18 11:28:29 +0200

    extracted REST resource classes into separate module

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask.ext.bcrypt import Bcrypt
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager, login_user, logout_user, current_user, login_required
-from flask_restful import Resource, Api, reqparse, fields, marshal_with
-from sqlalchemy.exc import IntegrityError
+from flask.ext.login import LoginManager
+from flask_restful import Api
 
 from app.config import BaseConfig
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 app.config.from_envvar('YAIA_CONFIG')
+
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+
+from app.models import *
+from app.resources import *
+
+api = Api(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -20,111 +27,12 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-api = Api(app)
-bcrypt = Bcrypt(app)
-db = SQLAlchemy(app)
-
-from app.models import *
-
-
-# skz: how to handle this with angular?
-# login_manager.login_view = 'login'
-
-
-@app.route('/')
-def index():
-    return app.send_static_file('views/index.html')
-
-
-class Users(Resource):
-    def post(self):
-        try:
-            parser = reqparse.RequestParser()
-            parser.add_argument('login', type=str, required=True,
-                                help='login for the new user')
-            parser.add_argument('email', type=str, required=True,
-                                help='email address for the new user')
-            parser.add_argument('password', type=str, required=True,
-                                help='password for the new user')
-            args = parser.parse_args()
-
-            user = User(login=args['login'],
-                        email=args['email'],
-                        password=args['password'])
-
-            db.session.add(user)
-            db.session.commit()
-            db.session.close()
-
-            return {'message': 'user created'}
-        except IntegrityError:
-            return {'message': 'user already exists'}, 409
-
-
-class Sessions(Resource):
-    def user_session(self):
-        return {'authenticated': True, 'id': current_user.get_id(), 'login': current_user.login, 'email': current_user.email}
-
-    def anonymous_session(self):
-        return {'authenticated': False, 'id': None, 'login': None, 'email': None}
-
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('login', type=str, required=True,
-                            help='login for the user')
-        parser.add_argument('password', type=str, required=True,
-                            help='password for the user')
-        args = parser.parse_args()
-
-        user = User.query.filter_by(login=args['login']).first()
-
-        if user is not None and user.check_password(args['password']):
-            login_user(user)
-            return self.user_session()
-        else:
-            return self.anonymous_session(), 403
-
-    @login_required
-    def get(self):
-        if current_user.get_id() is not None:
-            return [self.user_session()]
-        else:
-            return [self.anonymous_session()]
-
-    @login_required
-    def delete(self):
-        logout_user()
-        return {'message': 'OK'}
-
-
-class Invoices(Resource):
-    _fields = {
-        'id': fields.Integer,
-        'ref_num': fields.Integer,
-        'issued_on': fields.DateTime(dt_format='iso8601'),
-    }
-
-    @login_required
-    @marshal_with(_fields)
-    def get(self):
-        return Invoice.query.filter_by(owner_id=current_user.get_id()).order_by(Invoice.issued_on.desc()).all()
-
-
-class Customers(Resource):
-    _fields = {
-        'id': fields.Integer,
-        'name': fields.String,
-        'tax_id': fields.String,
-    }
-
-    @login_required
-    @marshal_with(_fields)
-    def get(self):
-        return Customer.query.filter_by(user_id=current_user.get_id()).all()
-
 api.add_resource(Users, '/api/users')
 api.add_resource(Sessions, '/api/sessions')
 api.add_resource(Invoices, '/api/invoices')
 api.add_resource(Customers, '/api/customers')
 
 
+@app.route('/')
+def index():
+    return app.send_static_file('views/index.html')
---
+++ app/resources.py
@@ -0,0 +1,101 @@
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask_restful import Resource, reqparse, fields, marshal_with
+from sqlalchemy.exc import IntegrityError
+
+from app import db
+from app.models import *
+
+
+class Users(Resource):
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('login', type=str, required=True,
+                                help='login for the new user')
+            parser.add_argument('email', type=str, required=True,
+                                help='email address for the new user')
+            parser.add_argument('password', type=str, required=True,
+                                help='password for the new user')
+            args = parser.parse_args()
+
+            user = User(login=args['login'],
+                        email=args['email'],
+                        password=args['password'])
+
+            db.session.add(user)
+            db.session.commit()
+            db.session.close()
+
+            return {'message': 'user created'}
+        except IntegrityError:
+            return {'message': 'user already exists'}, 409
+
+
+class Sessions(Resource):
+    def user_session(self):
+        return {'authenticated': True,
+                'id': current_user.get_id(),
+                'login': current_user.login,
+                'email': current_user.email}
+
+    def anonymous_session(self):
+        return {'authenticated': False,
+                'id': None,
+                'login': None,
+                'email': None}
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('login', type=str, required=True,
+                            help='login for the user')
+        parser.add_argument('password', type=str, required=True,
+                            help='password for the user')
+        args = parser.parse_args()
+
+        user = User.query.filter_by(login=args['login']).first()
+
+        if user is not None and user.check_password(args['password']):
+            login_user(user)
+            return self.user_session()
+        else:
+            return self.anonymous_session(), 403
+
+    @login_required
+    def get(self):
+        if current_user.get_id() is not None:
+            return [self.user_session()]
+        else:
+            return [self.anonymous_session()]
+
+    @login_required
+    def delete(self):
+        logout_user()
+        return {'message': 'OK'}
+
+
+class Invoices(Resource):
+    _fields = {
+        'id': fields.Integer,
+        'ref_num': fields.Integer,
+        'issued_on': fields.DateTime(dt_format='iso8601'),
+    }
+
+    @login_required
+    @marshal_with(_fields)
+    def get(self):
+        return Invoice.query.filter_by(
+            owner_id=current_user.get_id()).order_by(
+                Invoice.issued_on.desc()).all()
+
+
+class Customers(Resource):
+    _fields = {
+        'id': fields.Integer,
+        'name': fields.String,
+        'tax_id': fields.String,
+    }
+
+    @login_required
+    @marshal_with(_fields)
+    def get(self):
+        return Customer.query.filter_by(user_id=current_user.get_id()).all()
