37f9e0ab75e00ae75133d329795ae4a452eaf4a0
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2022-12-01 13:39:27 +0100

    Invoice export improvements
    - hide requester part if not filled
    - support markdown-style bolding in notes

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, Markup, render_template
 from flask_bower import Bower
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy as SQLAlchemyBase
 from flask_login import LoginManager
 from flask_restful import Api
+import re
 
 from app.config import BaseConfig
 
@@ -46,6 +47,9 @@ def load_user(id):
 def date_filter(s):
     return s.split('T')[0] if s is not None else s
 
+@app.template_filter('md')
+def markdown(s):
+    return Markup(re.sub(r'\*(.+?)\*', r'<b>\1</b>', s)) if s is not None else s
 
 api.add_resource(Users, '/api/users')
 api.add_resource(Sessions, '/api/sessions')
